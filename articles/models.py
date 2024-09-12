7013f0de1748322c954899d75d0881a32f47bc24
Dino <swlee0609@gmail.com>
2024-09-12 20:37:35 +0900

    test: article 모델 업로드, 잘되는지 확인할게요
    - articles 앱 article 모델 기본만 구현

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Article(TimeStampedModel):
+    title = models.CharField(max_length=25)
+    content = models.TextField()
+    url = models.URLField(blank=True)
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="user_articles",  # 필요없으면 삭제하자
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-id"]  # 저장할 때 역순으로 정렬
