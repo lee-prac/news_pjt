7714d9cb4ba64975958f62d98d6bf286ad36235d
Dino <swlee0609@gmail.com>
2024-09-13 14:44:23 +0900

    feature: articles CRUD
    - create, read, update, delete 기본로직 구현

--- articles/urls.py
+++ articles/urls.py
@@ -1,2 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ArticleViewSet
+
+router = DefaultRouter()
+router.register(r"", ArticleViewSet)
+
 urlpatterns = [
+    path("", include(router.urls)),
 ]
+
+
+# api/articles/
+# GET: 목록 / POST: 생성
+
+# api/articles/<pk>/
+# GET: 상제 / PUT: 수정 / DELETE: 삭제
