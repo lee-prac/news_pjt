de1627631f61362cbc8e4ed26ebd5d757ac7c8b1
mesurechiffon <dri0205@naver.com>
2024-09-16 20:54:47 +0900

    feature:signup

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,19 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
-
-class CustomUser(AbstractUser):
+class CustomUser(AbstractBaseUser):
+    username = None
+    user_id = models.CharField(max_length=10, unique=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=10)
     nickname = models.CharField(max_length=10)
     bio = models.TextField(blank=True)
 
+    USERNAME_FIELD = 'user_id'
+    REQUIRED_FIELDS = ['email']
+
+    objects = CustomUserManager()
+
     def __str__(self):
-        return self.username
+        return self.user_id
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,23 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import CustomUser
+from .validators import validate_user_data
 from .serializers import SignupSerializer
 
+class SignupView(APIView):
+    def post(self, request):
+        rlt_message = validate_user_data(request.data)
+        if rlt_message is not None:
+            return Response({"message": rlt_message}, status=400)
+        
+        user = CustomUser.objects.create_user(
+            user_id=request.data.get("user_id"),
+            password=request.data.get("password"),
+            name=request.data.get("name"),
+            nickname=request.data.get("nickname"),
+            email=request.data.get("email"),
+            bio=request.data.get("bio")
+        )
 
-# 회원가입 뷰
-class SignupView(generics.CreateAPIView):
-    serializer_class = SignupSerializer
+        serializer = SignupSerializer(user)
+        return Response(serializer.data)
